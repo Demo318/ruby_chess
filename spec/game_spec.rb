751f654de8811c9ae1432807f71462061c718a1f
Devin Mork <devin.mork@gmail.com>
2018-05-16 08:06:35 -0500

    Add game_spec.rb

---
+++ spec/game_spec.rb
@@ -0,0 +1,5 @@
+# This file will be for testing the main Game class.
+# Game module is analogous to the game 'rulebook.'
+# The board played on, the peices played with, and the players themselves
+# will each receive their own classes. Win conditions will be stores in their own
+# module to be included in the Game class.
