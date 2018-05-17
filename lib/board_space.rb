eb0141a99fa4f84b622d79599a2efd19c50a5117
Devin Mork <devin.mork@gmail.com>
2018-05-16 19:37:06 -0500

    BoardSpace intialize with coordinates passed.

--- lib/board_space.rb
+++ lib/board_space.rb
@@ -1,3 +1,10 @@
+# The BoardSpace class represents each square of the chess board.
+# Each space knows its own coordinates, what piece is on it,
+# and which spaces are adjacent to it.
 class BoardSpace
+  attr_reader :coordinates
 
+  def initialize(x_coord, y_coord, adjacent_spaces = [nil, nil, nil, nil])
+    @coordinates = [x_coord, y_coord]
+  end
 end
--- spec/board_space_spec.rb
+++ spec/board_space_spec.rb
@@ -4,4 +4,9 @@ require_relative '../lib/board_space.rb'
 
 describe BoardSpace do
 
+  describe '.initialize' do
+    before { @this_space = BoardSpace.new(0, 0) }
+    it { expect(@this_space.coordinates).to eq([0, 0]) }
+  end
+
 end
