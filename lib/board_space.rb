dc02a14075f21466e45bc31d225c028eaec8701d
Devin Mork <devin.mork@gmail.com>
2018-05-16 19:44:43 -0500

    Test BoardSpace with no adjacent spaces.

--- lib/board_space.rb
+++ lib/board_space.rb
@@ -2,9 +2,14 @@
 # Each space knows its own coordinates, what piece is on it,
 # and which spaces are adjacent to it.
 class BoardSpace
-  attr_reader :coordinates
+  attr_reader :coordinates, :move_up, :move_right,
+              :move_down, :move_left
 
   def initialize(x_coord, y_coord, adjacent_spaces = [nil, nil, nil, nil])
     @coordinates = [x_coord, y_coord]
+    @move_up = adjacent_spaces[0]
+    @move_right = adjacent_spaces[1]
+    @move_down = adjacent_spaces[2]
+    @move_left = adjacent_spaces[3]
   end
 end
--- spec/board_space_spec.rb
+++ spec/board_space_spec.rb
@@ -5,8 +5,21 @@ require_relative '../lib/board_space.rb'
 describe BoardSpace do
 
   describe '.initialize' do
-    before { @this_space = BoardSpace.new(0, 0) }
-    it { expect(@this_space.coordinates).to eq([0, 0]) }
-  end
+    context 'when passed x and y coordinates' do
+      before do 
+        @this_space = BoardSpace.new(0, 0)
+        @that_space = BoardSpace.new(4, 4)
+      end
+      it { expect(@this_space.coordinates).to eq([0, 0]) }
+      it { expect(@that_space.coordinates).to eq([4, 4]) }
+    end
 
+    context 'when passed not adjacent spaces' do
+      before { @this_space = BoardSpace.new(1, 1) }
+      it { expect(@this_space.move_up).to be_nil }
+      it { expect(@this_space.move_right).to be_nil }
+      it { expect(@this_space.move_down).to be_nil }
+      it { expect(@this_space.move_left).to be_nil }
+    end
+  end
 end
