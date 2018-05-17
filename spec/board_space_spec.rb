c33f4f37a6c20ac221496a32f4a5bb5b7925775d
Devin Mork <devin.mork@gmail.com>
2018-05-16 19:52:26 -0500

    Test adjacent space assignment for BoardSpace.

--- spec/board_space_spec.rb
+++ spec/board_space_spec.rb
@@ -21,5 +21,24 @@ describe BoardSpace do
       it { expect(@this_space.move_down).to be_nil }
       it { expect(@this_space.move_left).to be_nil }
     end
+
+    context 'when passed individual adjacent spaces' do
+      before do
+        @space_with_up = BoardSpace.new(4, 4, [BoardSpace.new(4, 5),
+                                               nil, nil, nil])
+        @space_with_right = BoardSpace.new(4, 4, [nil,
+                                                  BoardSpace.new(5, 4),
+                                                  nil, nil])
+        @space_with_down = BoardSpace.new(4, 4, [nil, nil,
+                                                 BoardSpace.new(4, 3),
+                                                 nil])
+        @space_with_left = BoardSpace.new(4, 4, [nil, nil, nil,
+                                                 BoardSpace.new(3, 4)])
+      end
+      it { expect(@space_with_up.move_up.coordinates).to eq([4, 5]) }
+      it { expect(@space_with_right.move_right.coordinates).to eq([5, 4]) }
+      it { expect(@space_with_down.move_down.coordinates).to eq([4, 3]) }
+      it { expect(@space_with_left.move_left.coordinates).to eq([3, 4]) }
+    end
   end
 end
