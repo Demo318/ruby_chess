2bf7fa0bd45285a4f4aa7e71d27e9ba031b66ba7
Devin Mork <devin.mork@gmail.com>
2018-05-21 22:28:48 -0500

    Start Board.new functionality.

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,11 @@
+# An instance of Board is a graph of BoardSpace objects. These
+# are interrelated to one another and are most fundamentally accessed
+# through the @root_space at coordinates 0, 0. Functions of the
+# board include finding and returning BoardSpace objects.
 class Board
+  attr_reader :root_space
 
+  def initialize
+    @root_space = BoardSpace.new(0, 0)
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,6 +3,13 @@ require 'rspec'
 require_relative '../lib/board.rb'
 
 describe Board do
-
+  describe '.initialize' do
+    context 'when new board created' do
+      before do
+        @my_board = Board.new
+      end
+      it { expect(@my_board.root_space.class).to eq(BoardSpace) }
+    end
+  end
 
 end
