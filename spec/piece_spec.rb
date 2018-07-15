97e2f7828e456dea6118ff42806582f9005bedc7
Devin Mork <devin.mork@gmail.com>
2018-07-14 22:40:16 -0500

    Add tests for initializing all piece names.

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -12,5 +12,42 @@ describe Piece do
       it { expect(@this_piece.number_of_moves).to eq(0) }
       it { expect(@this_piece.move_rules_name).to eq('knight') }
     end
+    context 'when passed arguments to be a Knight on team 2' do
+      before do
+        @this_piece = Piece.new(2, 'knight')
+      end
+      it { expect(@this_piece.team).to eq(2) }
+    end
+    context 'when passed arguments to be a Rook' do
+      before do
+        @this_piece = Piece.new(1, 'rook')
+      end
+      it { expect(@this_piece.team).to eq(1) }
+      it { expect(@this_piece.move_rules_name).to eq('rook') }
+    end
+    context 'when passed arguments to be a Bishop' do
+      before do
+        @this_piece = Piece.new(1, 'bishop')
+      end
+      it { expect(@this_piece.move_rules_name).to eq('bishop') }
+    end
+    context 'when passed arguments to be a Pawn' do
+      before do
+        @this_piece = Piece.new(1, 'pawn')
+      end
+      it { expect(@this_piece.move_rules_name).to eq('pawn') }
+    end
+    context 'when passed arguments to be a King' do
+      before do
+        @this_piece = Piece.new(1, 'king')
+      end
+      it { expect(@this_piece.move_rules_name).to eq('king') }
+    end
+    context 'when passed arguments to be a Queen' do
+      before do
+        @this_piece = Piece.new(1, 'queen')
+      end
+      it { expect(@this_piece.move_rules_name).to eq('qyeen') }
+    end
   end
 end
