e7d45a151350e60cb6f335e04773bf29df952374
Devin Mork <devin.mork@gmail.com>
2018-07-14 22:23:46 -0500

    Create tests for initializing first Piece object.

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -3,6 +3,14 @@ require 'rspec'
 require_relative '../lib/piece.rb'
 
 describe Piece do
-
-
+  describe '.initialize' do
+    context 'when passed arguments to be a Knight on team 1' do
+      before do
+        @this_piece = Piece.new(1, "knight")
+      end
+      it { expect(@this_piece.team).to eq(1) }
+      it { expect(@this_piece.no_of_moves.to eq(0)) }
+      it { expect(@this_piece.move_rules_name).to eq("knight") }
+    end
+  end
 end
