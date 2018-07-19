a4767c8069d5c2c907016055ef081440d16695d9
Devin Mork <devin.mork@gmail.com>
2018-07-18 22:14:05 -0500

    Bishop can find available moves on empty board, all tests passing.

--- lib/piece_modules/bishop.rb
+++ lib/piece_modules/bishop.rb
@@ -4,6 +4,32 @@ module Bishop
     'bishop'
   end
 
-  
+  def find_available_moves(own_space)
+    available_spaces_coords = []
+    available_spaces_coords = check_direction(:move_up, :move_right,
+                                              own_space,
+                                              available_spaces_coords)
+    available_spaces_coords = check_direction(:move_down, :move_right,
+                                              own_space,
+                                              available_spaces_coords)
+    available_spaces_coords = check_direction(:move_down, :move_left,
+                                              own_space,
+                                              available_spaces_coords)
+    available_spaces_coords = check_direction(:move_up, :move_left,
+                                              own_space,
+                                              available_spaces_coords)
+    available_spaces_coords
+  end
+
+  private
+
+  def check_direction(one_direct, two_direct, space, available_arr)
+    loop do
+      break if space.send(one_direct).nil? || space.send(one_direct).send(two_direct).nil?
+      space = space.send(one_direct).send(two_direct)
+      available_arr << space.coordinates
+    end
+    available_arr
+  end
 
 end
