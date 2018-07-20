2121822a4ea56b150f2f7d350c5b974e6f21381e
Devin Mork <devin.mork@gmail.com>
2018-07-19 21:19:46 -0500

    Outline structure of tests needed for Pawn#find_available_moves.

--- spec/piece_modules_spec/pawn_spec.rb
+++ spec/piece_modules_spec/pawn_spec.rb
@@ -3,12 +3,95 @@ require 'rspec'
 require_relative '../../lib/piece.rb'
 
 describe Pawn do
-  context 'when creating a pawn for each team' do
-    before do
-      @team_one_pawn = Piece.new(1, 'pawn')
-      @team_two_pawn = Piece.new(2, 'pawn')
+  describe '.initialize' do
+    context 'when creating a pawn for each team' do
+      before do
+        @team_one_pawn = Piece.new(1, 'pawn')
+        @team_two_pawn = Piece.new(2, 'pawn')
+      end
+      it { expect(@team_one_pawn.team).to eq(1) }
+      it { expect(@team_two_pawn.team).to eq(2) }
     end
-    it { expect(@team_one_pawn.team).to eq(1) }
-    it { expect(@team_two_pawn.team).to eq(2) }
   end
+
+
+  describe '#find_available_moves' do
+    before do
+      @my_board = Board.new
+      @my_pawn = Piece.new(1, 'pawn')
+    end
+    context 'on first move' do
+      before do
+        # put two pawns of each team in proper starting space
+      end
+      #pawn can move two spaces forward on first move.
+      context 'when the board is empty' do
+        it do
+          expect().to eq([])
+        end
+      end
+
+      context 'when team member & enemy blocking' do
+        before do
+          # have one blocked by teammate
+          # have the other blocked by enemy
+        end
+        it do
+          expect().to eq([])
+        end
+      end
+
+      context 'when enemy can be captured' do
+        before do
+          # Create two pawns
+          # have one capture left and the other capture right.
+        end
+        it do
+          expect().to eq()
+        end
+      end
+    end
+
+    context 'not on first move' do
+      before do
+        # put two pawns of each team in new positions
+      end
+      #pawn can move two spaces forward on first move.
+      context 'when the board is empty' do
+        it do
+          expect().to eq([])
+        end
+      end
+
+      context 'when team member & enemy blocking' do
+        before do
+          # have one blocked by teammate
+          # have the other blocked by enemy
+        end
+        it do
+          expect().to eq([])
+        end
+      end
+
+      context 'when enemy can be captured' do
+        before do
+          # Create two pawns
+          # have one capture left and the other capture right.
+        end
+        it do
+          expect().to eq()
+        end
+      end
+
+      context 'when en passant capture available' do
+        before do
+          # setup circumstances, for each side, of en passant capture
+        end
+        it { expect().to eq() }
+      end
+    end
+  end
+
+  
 end
+
