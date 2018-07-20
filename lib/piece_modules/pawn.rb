ce328f7fa47197f8fbc8d82ed37e28e92f824f4e
Devin Mork <devin.mork@gmail.com>
2018-07-19 21:56:46 -0500

    Passing test Pawn#find_available_moves 'on first move' 'when board is empty'

--- lib/piece_modules/pawn.rb
+++ lib/piece_modules/pawn.rb
@@ -1,11 +1,41 @@
 module Pawn
-
   def move_rules_name
     'pawn'
   end
 
-  def find_available_moves
-    nil
+  def find_available_moves(own_space)
+    moving = if @team == 1
+               :move_up
+             else
+               :move_down
+             end
+    available_spaces_coords = []
+    move_forward?(own_space, moving).each do |n|
+      available_spaces_coords << n
+    end
+    available_spaces_coords
+  end
+
+  private
+
+  def move_forward?(space, moving)
+    forward_moves = []
+    x = if @number_of_moves.zero?
+          2
+        else
+          1
+        end
+    x.times do
+      break unless valid_next_space?(space, moving)
+      space = space.send(moving)
+      forward_moves << space.coordinates
+    end
+    forward_moves
+  end
+
+  def valid_next_space?(space, moving)
+    return false if space.send(moving).nil?
+    true
   end
 
 end
